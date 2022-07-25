c25b3244a5476d8948fa8004f100aeaf00209631
Itay <weinbergitay@gmail.com>
2022-07-25 14:03:58 +0300

    modifying PORT

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3050;
 app.use(require("cors")());
 app.use(express.json());
 // app.use("/files", require("./fileRouter"));
