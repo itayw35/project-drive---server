f799781e33d466650d9171367be4c3114992e54b
Itay <weinbergitay@gmail.com>
2022-07-28 23:03:14 +0300

    adding legal file types

--- middleware/isValidExtension.js
+++ middleware/isValidExtension.js
@@ -2,7 +2,11 @@ async function isValidExtension(req, res, next) {
   try {
     const fileName = req.file ? req.file.originalname : req.body.newName;
     const ext = fileName.slice(fileName.lastIndexOf(".") + 1);
-    if (["pdf", "txt", "doc", "pages", "docx"].find((char) => ext == char)) {
+    if (
+      ["pdf", "txt", "doc", "pages", "docx", "jpg", "jpeg", "HEIF"].find(
+        (char) => ext == char
+      )
+    ) {
       next();
     } else {
       throw { code: 400, message: "illegal file type" };
